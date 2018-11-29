62cc0e2bd7a418f8f0bfa785966c36944bec08d7
Jess Gehring <34904627+JessG96@users.noreply.github.com>
2018-11-29 17:41:10 -0500

    Update Menu_Screen.swift
    Menu list works, currently working on trying to get the search to work, almost there woo!

--- Menu_Screen.swift
+++ Menu_Screen.swift
@@ -8,24 +8,52 @@ class Menu_Screen: UIViewController, UITableViewDelegate, UITableViewDataSource 
   //Search Bar Connection on MainStoryBoard
   @IBOutlet weak var MenuSearch: UISearchBar!
   
+  //TableView Conenction on MainStoryBoard
+  @IBOutlet weak var MenuList: UITableView!
+  
   //Creating an array to hold all the items that can be purchased
-  var list = ["Coffee", "Salad") //Currently a basic idea, somehow want to go back and change this so we can have categories, but for now this works
+  var list = ["Coffee", "Salad", "Bacon", "Egg", "Cappuccino", "Zebra", "Expresso"] //Currently a basic idea, somehow want to go back and change this so we can have categories, but for now this works
+  
+  //Creating an array to put the search item in
+  var SearchItem = [String]()
+  
+  //Creating a bool variable
+  var Searching = false
+  
   
   //========FUNCTIONS==================================================================================
   
   public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
   {
-    return(list.count)
+    if Searching
+    {
+     return(SearchingItem.count) 
+    } else {
+      return(list.count)
+    }
   }
   
   public func tableView(_ tableView: UITablView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
   {
-    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell"
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
     
-    cell.textLabel?.text = list[indexPath.row]
+    if Searching {
+      cell.textLabel?.text = SearchItem[indexPath.row]
+    } 
+    else {
+      cell.textLabel?.text = list[indexPath.row]
+    }
     
     return(cell)
   }
+  
+  public func MenuSearch(_ MenuSearch: UISearchBar, textDidChange searchText: String)
+  {
+    SearchItem = list.filter({$0.prefix(searchText.count) == searchText})
+    Searching = true
+    MenuList.reloadData()
+  }
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
