01efe4dc11ab8e8af1fddd5b2fefda9afb245360
SShahabb <ssalehi@kent.edu>
2018-12-07 18:14:00 -0500

    added Jess' code to the menu file

--- Appuccino_Test/Menu_Screen.swift
+++ Appuccino_Test/Menu_Screen.swift
@@ -2,31 +2,31 @@
 //  Menu_Screen.swift
 //  Appuccino_Test
 //
-//  Created by Jess on 12/7/18.
-//  Copyright © 2018 Jess. All rights reserved.
+//  Created by Guest Jess on 12/7/18.
+//  Copyright Â© 2018 Jess. All rights reserved.
 //
 
 import UIKit
+import Mapkit
 
-class Menu_Screen: UIViewController // UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate
-{
-    //references to other view controllers
-    var MapVC: Map_Screen?
-    //var 3rd screen
-    
-    //object for annotation segue
-    var pin: CoffeeShop!
-    
+class Menu_Screen: UIViewController { //UITableViewController    
     @IBOutlet weak var Points: UILabel!
-    
     @IBOutlet weak var MenuSearch: UISearchBar!
-    
     @IBOutlet weak var MenuList: UITableView!
+   
+    let list = [
+        Coffee: "Drip Coffee", "Mocha", "Latte", "Cappuccino", "Americano", "Espresso", "Iced Mocha", "Iced Latte", "Cold Brew", "Frozen Mocha", "Frappe"
+        Chai Latte, "Hot Tea", "Iced Tea",
+        Misc.: "Italian Soda", "Cider", 
+        Smoothies: "Smoothie-Strawberry", "Smoothie-Strawberry/Banana", "Peach", "Wild Berry", "Pina Colada", 
+        Bakery: "Bagel W/Cream Cheese", "Bagel W/Jam", "Plain Bagel", "Biscotti", "Brownies", "Cinnamon Rolls", "Cookies", "Muffins", "Scones", "Strudel", 
+        Breakfast: "Yogurt Parfait", "Breakfast Sammich", "Breakfast Wrap", "Avocado Toast", "Santa Fe Toast"
+    ]
     
-    var list = ["Coffee", "Bagel", "Bacon", "Cappuccino", "Apple", "Candy", "Xray", "Banana", "Expresso", "Zebra"]
+    var itemArray = [String]() 
     var SearchItem = [String]()
     var Searching = false
-
+    var index: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,71 +34,58 @@ class Menu_Screen: UIViewController // UITableViewDelegate, UITableViewDataSourc
         // Do any additional setup after loading the view.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
 
 extension Menu_Screen: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.section[section]
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.section.count //was return 1
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if Searching {
             return SearchItem.count
         } else {
-            return list.count
-        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let Cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
-        
-        if Searching {
-            Cell.textLabel?.text = SearchItem[indexPath.row]
-        } else {
-            Cell.textLabel?.text = list[indexPath.row]
-        }
-        return Cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        var index: Int = 0
-        var trackPoints: [Int] = [index]
-        
-        func setPoints(pointCount: Int) {
-            if(index == 10) {
-                index = 0
-            }
-        
-            trackPoints[index] = pointCount
-            //index += 1
-        }
-        
-        let tempPoints: [Int] = trackPoints
-        let pointIndex = "Your Appuccino Points: \(tempPoints)"
-        
-        switch self.list[indexPath.row] {
-            
-            case "Coffee":
-                self.Points.text = pointIndex
-            
-            case "Cappuccino":
-                self.Points.text = pointIndex
-            
-            default:
-              print("random random")
-            }
-        
-            tableView.deselectRow(at: indexPath, animated: true)
+            return self.list[section].count
         }
     }
 
-extension Menu_Screen: UISearchBarDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let Cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        if Searching {
+            Cell.textLabel?.text = SearchItem[indexPath.row]
+        } else {
+            Cell.textLabel?.text = list[indexPath.section][indexPath.row]
+        }
+        return Cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        func setPoints() -> Int {
+            if(index == 10) {
+                index = 0
+            }
+            index += 1
+            return(index)
+        }
+        
+        let pointIndex = "Your Appuccino Points: \(setPoints())"
+        
+        for food in list {
+            if food.key == "Coffee" {
+                self.Points.text = pointIndex
+        }
+        
+    }
+}
     
+extension Menu_Screen: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        SearchItem = list.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+        //SearchItem = list.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+        var searchResult = list.flatMap({$0}).filter {$0.lowercased().contains(searchText)}
         Searching = true
         MenuList.reloadData()
     }
@@ -108,8 +95,6 @@ extension Menu_Screen: UISearchBarDelegate {
         searchBar.text = ""
         MenuList.reloadData()
     }
-    
-    
 }
 
 
