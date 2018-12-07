26a856e85137e419c0c930141bdf0f2c88ace7c3
SShahabb <ssalehi@kent.edu>
2018-12-06 20:49:31 -0500

    added more map functionality

--- Appuccino_Test/Map_Screen.swift
+++ Appuccino_Test/Map_Screen.swift
@@ -46,10 +46,6 @@ class Map_Screen: UIViewController {
                                       locationName: "KSU Library",
                                       discipline: "Busy",
                                       coordinate: CLLocationCoordinate2D(latitude: 41.1467, longitude: -81.3424)))
-        coffeeShops.append(CoffeeShop(title: "San Fran Test",
-                                      locationName: "Downtown SF - Why is my current location here??",
-                                      discipline: "Annoying",
-                                      coordinate: CLLocationCoordinate2D(latitude: 37.7881, longitude: -122.4046)))
     }
     
     //move position and zoom
@@ -63,11 +59,12 @@ class Map_Screen: UIViewController {
     //make a function to add pins/shops to the array in addition to the original ones
     //maybe this can go in 3rd screen and attach it to a button on this screen
     
-    //make a function to zoom in and out if its not built in
-    //the library mac and mouse wouldnt let me zoom
-    //there must be a way to do this without coding it
+    //make a function to search for places
     
     //make a function to segue between screens when clicking on annotation or a popup after the annotation
+    
+    //manually move to new location, dont think i ever need to do this, maybe jump to a desired shop?
+    //moveMap(lat: lat, lon: lon, latD: latD, lonD: lonD)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,18 +80,17 @@ class Map_Screen: UIViewController {
         //ConfigVC = viewController2 as? Config_Screen
         
         //setup delegates
+        mapView.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters //AccuracyTenMeters //AccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
-        //mapView.delegate = self ?
         
         //populate and add pins for shops
+        //addShops() //adds any user added custom shops/locations
         makeShops()
         for shop in coffeeShops { mapView.addAnnotation(shop) }
         
-        //manually move to new location
-        //moveMap(lat: lat, lon: lon, latD: latD, lonD: lonD)
     }
 
     override func didReceiveMemoryWarning() {
@@ -106,12 +102,13 @@ class Map_Screen: UIViewController {
 
 //extension for delegate to access and handle current location
 extension Map_Screen: CLLocationManagerDelegate {
+    //authorizes location access
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
             locationManager.requestLocation()
         }
     }
-    
+    //sets initial map location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             let span = MKCoordinateSpanMake(0.075, 0.075)
@@ -119,11 +116,40 @@ extension Map_Screen: CLLocationManagerDelegate {
             mapView.setRegion(region, animated: true)
         }
     }
-    
+    //error handler
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
         print("error:: \(error.localizedDescription)")
     }
     
 }
+
+//extension for annotation popup
+extension Map_Screen: MKMapViewDelegate {
+    //creates pop up window when clicking on a coffee shop annotation
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? CoffeeShop else { return nil }
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+    
+    //create func that sends you to menu when clicking on the i after clicking on annotation
+}
+
+
+
+
+
+
 
 
